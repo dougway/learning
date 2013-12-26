901ff6d1e55eb2ac30df6df9cb7eb2ef794370f2
Douglas Way <magway@optonline.net>
2013-12-26 12:48:41 -0500

    first commit

---
+++ dougtest.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/doug' do
+	@x = "world"
+	erb "<br><b>Hello</b> <%= Time.now %> "
+end
+
+get '/way/:x' do 
+	erb "Hello  #{params[:x]}" 
+end
+
+get '/harpo' do
+	@x = "dog" 
+	erb :foo
+end
